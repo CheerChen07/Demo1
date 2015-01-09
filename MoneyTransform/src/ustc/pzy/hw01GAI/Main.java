8afae848244cc1d27b04efe312f4881fd1ea4ba8
CheerChen07 <chqny@sina.com>
2015-01-09 23:20:39 +0800

    ssssss
    asdasd

--- MoneyTransform/src/ustc/pzy/hw01GAI/Main.java
+++ MoneyTransform/src/ustc/pzy/hw01GAI/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void  main(String[] args) {
 		// TODO Auto-generated method stub
+//haahhhaha
 		Transform tf=new Transform();
 
 		System.out.println("请输入一个阿拉伯金额");
